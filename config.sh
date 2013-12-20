1f9d696a7bdf984100c47463f3ebba16c24ad3e7
Vishnu Sreekumar <vishnu.srkmr@gmail.com>
2013-12-20 13:35:38 +0530

    Modified the AS group creation scripts to include SNS notification configuration also.

--- config.sh
+++ config.sh
@@ -6,10 +6,11 @@
 # SECURITY_KEY :
 # AZ : Availability Zones
 
-export NAME_TAG=
-export SPOT_PRICE=
-export AMI=
-export INSTANCE_TYPE=
-export AZ=
-export SECURITY_KEY=
-export SECURITY_GROUP=
+export NAME_TAG=12202013-test
+export SPOT_PRICE=0.01
+export AMI=ami-85a08cec
+export INSTANCE_TYPE=t1.micro
+export AZ=us-east-1a
+export SECURITY_KEY=vishnus-test-key
+export SECURITY_GROUP=spot-monitor-test
+export SNS_ARN=arn:aws:sns:us-east-1:547078464708:spot-group-scaling-alerts
--- create_as_group.sh
+++ create_as_group.sh
@@ -13,7 +13,13 @@ as-create-launch-config spot-lc-${NAME_TAG} --image_id ${AMI} --group ${SECURITY
 as-create-launch-config ondemand-lc-${NAME_TAG} --image_id ${AMI} --group ${SECURITY_GROUP} --instance-type ${INSTANCE_TYPE} --key ${SECURITY_KEY}
 
 #Create auto-scaling groups
-as-create-auto-scaling-group spot-${NAME} --launch-configuration spot-lc-${NAME_TAG} --availability-zones ${AZ} --default-cooldown 600 --min-size 1 --max-size 2
-as-create-auto-scaling-group ondemand-${NAME} --launch-configuration ondemand-lc-${NAME_TAG} --availability-zones ${AZ} --default-cooldown 600 --min-size 0 --max-size 2
+as-create-auto-scaling-group spot-${NAME} --launch-configuration spot-lc-${NAME_TAG} --availability-zones ${AZ} --default-cooldown 600 --min-size 1 --desired-capacity 2 --max-size 3
+as-create-auto-scaling-group ondemand-${NAME} --launch-configuration ondemand-lc-${NAME_TAG} --availability-zones ${AZ} --default-cooldown 600 --min-size 0 --desired-capacity 0 --max-size 2
+
+#Define notification configuration
+for group in spot ondemand
+do
+  as-put-notification-configuration ${group}-${NAME} -t "${SNS_ARN}" -n autoscaling:EC2_INSTANCE_LAUNCH, autoscaling:EC2_INSTANCE_TERMINATE
+done
 
 #END
